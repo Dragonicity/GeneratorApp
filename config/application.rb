af8fc57f1ff3e504918997d38887ebdad99eaaae
Mark-Nelson <mark.nelson@dragonicity.com>
2017-12-09 09:47:05 +0800

    Add scaffold template for index

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,12 @@ module GeneratorApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :test_unit, fixture: true
+      g.stylesheets     false
+      g.javascripts     false 
+    end
   end
 end
